3582e9fe5e4982214db3ad341b174afe2a04767f
davidbastian <d@davidbastian.red>
2021-05-14 12:53:32 +0200

    update minicss plugin

--- app/src/views/about/view.js
+++ app/src/views/about/view.js
@@ -68,7 +68,7 @@ class AboutView {
                <a href="https://twitter.com/davidbastian_cl" target="_blank">Twitter</a>
                <a href="https://www.instagram.com/davidbastian" target="_blank">Instagram</a>
                <a href="https://cl.linkedin.com/in/davidbastianf" target="_blank">LinkedIn</a>
-               <a class="hide" href="common/doc/davidbastiancv.pdf" target="_blank">CV</a>
+               <a href="common/doc/davidbastiancv.pdf" target="_blank">CV</a>
             </div>
         </div>
       `;
--- webpack.config.js
+++ webpack.config.js
@@ -4,82 +4,96 @@ const CopyWebpackPlugin = require("copy-webpack-plugin")
 var ip = require("ip")
 
 module.exports = {
-    // Where files should be sent once they are bundled
-    entry: "./app/index.js",
-    output: {
-        filename: "bundle.js",
-        path: path.resolve(__dirname, "dist")
-    },
+  // Where files should be sent once they are bundled
+  entry: "./app/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist")
+  },
 
-    devServer: {
-        contentBase: path.join(__dirname, "dist"),
-       // compress: true,
-     //   disableHostCheck: true, // That solved it
-        port: 9293,
-        host: ip.address()
-    },
-    plugins: [
-        new MiniCssExtractPlugin({
-            filename: 'style.css',
-        }),
-        new CopyWebpackPlugin({
-            patterns: [{ from: "app/index.html" },
-            { from: "app/common/media/", to: "common/media/" },
-            { from: "app/common/social/", to: "common/social/" },
-            { from: "app/common/doc/", to: "common/doc/" },
-            { from: "app/favicon.png" }
-            ]
-        })
-    ],
-    module: {
-        rules: [{
-                test: /\.(js|jsx)$/,
-                exclude: /nodeModules/,
-                use: {
-                    loader: 'babel-loader'
-                }
-            },
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    compress: true,
+    disableHostCheck: true, // That solved it
+    port: 9293,
+    host: ip.address()
+  },
+ 
+  module: {
+    rules: [{
+        test: /\.(js|jsx)$/,
+        exclude: /nodeModules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
 
-            {
-                test: /\.(png|jpe?g|gif)$/i,
-                loader: 'file-loader',
-                options: {
-                    name: "common/media/images/[name].[ext]"
-                },
-            },
-            {
-                test: /\.(ttf|eot|woff|woff2)$/,
-                loader: "file-loader",
-                options: {
-                    name: "common/fonts/[name].[ext]"
-                }
-            },
-            {
-                test: /\.(mp3|wav)$/,
-                loader: "file-loader",
-                options: {
-                    name: "common/media/audio/[name].[ext]"
-                }
-            },
-            {
-                test: /\.(mp4)$/i,
-                loader: 'file-loader',
-                options: {
-                    name: "common/media/video/[name].[ext]"
-                },
-            },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        loader: 'file-loader',
+        options: {
+          name: "common/media/images/[name].[ext]"
+        },
+      },
+      {
+        test: /\.(ttf|eot|woff|woff2)$/,
+        loader: "file-loader",
+        options: {
+          name: "common/fonts/[name].[ext]"
+        }
+      },
+      {
+        test: /\.(mp3|wav)$/,
+        loader: "file-loader",
+        options: {
+          name: "common/media/audio/[name].[ext]"
+        }
+      },
+      {
+        test: /\.(mp4)$/i,
+        loader: 'file-loader',
+        options: {
+          name: "common/media/video/[name].[ext]"
+        },
+      },
 
-            {
-                test: /\.(csv|svg|html)$/,
-                use: "raw-loader"
-            },
+      {
+        test: /\.(csv|svg|html)$/,
+        use: "raw-loader"
+      },
 
 
-            {
-                test: /\.(scss|css)$/,
-                use: ["style-loader", "css-loader", "sass-loader"]
-            },
-        ]
-    },
+      {
+        test: /\.(scss|css)$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"]
+      },
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "style.css",
+    }),
+    new CopyWebpackPlugin({
+      patterns: [{
+          from: "app/index.html"
+        },
+        {
+          from: "app/common/media/",
+          to: "common/media/"
+        },
+        {
+          from: "app/common/social/",
+          to: "common/social/"
+        },
+        {
+          from: "app/common/doc/",
+          to: "common/doc/"
+        },
+        {
+          from: "app/favicon.png"
+        }
+      ]
+    })
+  ]
 
 }
